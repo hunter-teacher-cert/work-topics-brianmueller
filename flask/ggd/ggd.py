adff18f8a4cdd4886c1290ba85beeaf26c8b358e
Brian Mueller <brianscottmueller@gmail.com>
2022-02-21 13:27:14 -0500

    add can/color functionality

--- flask/ggd/ggd.py
+++ flask/ggd/ggd.py
@@ -2,6 +2,7 @@ from flask import Flask, request, session
 from flask import render_template
 
 # import random
+import re
 
 app = Flask(__name__)
 
@@ -12,8 +13,13 @@ def index():
 
 @app.route("/guess",methods=['POST'])
 def guess():
-    user_input = request.form['user_input']
-    user_color = 'red'
-    user_can = 'can'
+    user_input = request.form['user_input'].lower()
+    pattern = re.compile(r"(.)\1")
+    if re.search(pattern, user_input):
+        user_color = "green"
+        user_can = "can"
+    else:
+        user_color = "red"
+        user_can = "can't"
     
     return render_template("guess.html", user_input=user_input, user_color = user_color, user_can = user_can)
