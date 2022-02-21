616f301a04cf612ca5bf9f8546080f0fc2e97cf8
Brian Mueller <brianscottmueller@gmail.com>
2022-02-21 11:46:05 -0500

    restructure to follow z's boilerplate

--- flask/ggd/ggd.py
+++ flask/ggd/ggd.py
@@ -1,1 +1,11 @@
-from app import app
+from flask import Flask, request, session 
+from flask import render_template
+
+# import random
+
+app = Flask(__name__)
+
+# the "root" route
+@app.route("/")
+def index():
+  return "<h1>Hello World</h1>"
